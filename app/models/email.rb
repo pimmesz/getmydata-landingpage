000cd4321942e298dd7749f80a45ed75f67e070a
Pim Zonneveld <pimzonneveld@gmail.com>
2018-03-05 20:55:30 +0100

    Fixed landing page

--- app/models/email.rb
+++ app/models/email.rb
@@ -1,2 +1,6 @@
 class Email < ApplicationRecord
+  validates :email, presence: true
+  validates :email, uniqueness: true
+  validates :name, presence: true
+  validates :name, uniqueness: true
 end
